a5727e5fe2ca825108b54dfead6d0f6d98e378e3
huanglonghan <huanglonghan@moplus.live>
2017-06-03 19:20:01 +0800

    kotlin

---
+++ baseutils/src/main/java/com/ec/www/view/swiperecycler/base/dwadwa.kt
@@ -0,0 +1,21 @@
+package com.ec.www.view.swiperecycler.base
+
+/**
+ * Created by huang on 2017/6/3.
+ */
+
+class dwadwa {
+    internal var abc = 1
+
+    fun woca(): Int {
+        var a = 1
+        a++
+        a = a + 1
+        return a
+    }
+
+    companion object {
+        internal var a = 1
+        internal val b = 1
+    }
+}
